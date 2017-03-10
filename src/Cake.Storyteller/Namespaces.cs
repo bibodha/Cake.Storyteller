1cab7ca1ea73717eb8b272fbc1bd4742bf1cd93e
Bibodha Neupane <bibodha.neupane@extendhealth.com>
2017-03-10 15:20:29 -0700

    Added XML doc around storyteller flags. Added namespaces file for namespace documentation. Resolves #1

---
+++ src/Cake.Storyteller/Namespaces.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Cake.Storyteller
+{
+    /// <summary>
+    /// This namespace contains types related to running Storyteller tool
+    /// </summary>
+    [CompilerGenerated]
+    internal class Namespaces
+    {
+        
+    }
+}
--- src/Cake.Storyteller/StorytellerSettings.cs
+++ src/Cake.Storyteller/StorytellerSettings.cs
@@ -1,5 +1,4 @@
 ﻿using Cake.Core.Tooling;
-using StoryTeller.Model;
 
 namespace Cake.Storyteller
 {
@@ -8,32 +7,74 @@ namespace Cake.Storyteller
     /// </summary>
     public class StorytellerSettings : ToolSettings
     {
+        /// <summary>
+        /// Path to write out the results. Default is stresults.htm
+        /// </summary>
         [StorytellerFlag("results-path")]
         public string ResultsPath { get; set; }
+        /// <summary>
+        /// Optional. Runs only one workspace
+        /// </summary>
         [StorytellerFlag("workspace")]
         public string Workspace { get; set; }
+        /// <summary>
+        /// Optional. Excludes specs with any of the specified tags (comma-delimited)
+        /// </summary>
         [StorytellerFlag("exclude-tags")]
         public string ExcludeTags { get; set; }
+        /// <summary>
+        /// Open the results in a browser after the run. DO NOT DO THIS IN CI!
+        /// </summary>
         [StorytellerFlag("open")]
         public bool? Open { get; set; }
+        /// <summary>
+        /// WriteToText the performance data in CSV format to the specified path
+        /// </summary>
         [StorytellerFlag("csv")]
         public string Csv { get; set; }
+        /// <summary>
+        /// WriteToText the raw result information to JSON format at the specified path
+        /// </summary>
         [StorytellerFlag("json")]
         public string Json { get; set; }
+        /// <summary>
+        /// Dump the raw JSON history of the batch run to the specified path
+        /// </summary>
         [StorytellerFlag("dump")]
         public string Dump { get; set; }
+        /// <summary>
+        /// Specify a build target to force Storyteller to choose that profile. By default, ST will use 'Debug'
+        /// </summary>
         [StorytellerFlag("build")]
         public string Build { get; set; }
+        /// <summary>
+        /// Storyteller test mode profile for systems like Serenity that use this
+        /// </summary>
         [StorytellerFlag("profile")]
         public string Profile { get; set; }
+        /// <summary>
+        /// Optional. Default project timeout in seconds.
+        /// </summary>
         [StorytellerFlag("timeout")]
         public int? Timeout { get; set; }
+        /// <summary>
+        /// Optional. Only runs tests with desired <see cref="Lifecycle"/>
+        /// </summary>
         [StorytellerFlag("lifecycle")]
         public string Lifecycle { get; set; }
+        /// <summary>
+        /// Optional. Applies extra logging to see progress within TeamCity during CI runs
+        /// </summary>
         [StorytellerFlag("teamcity")]
         public bool? TeamCity { get; set; }
+        /// <summary>
+        /// Optional. Override the config file selection of the Storyteller test running AppDomain
+        /// </summary>
         [StorytellerFlag("config")]
         public string Config { get; set; }
+        /// <summary>
+        /// Sets a minimum number of retry attempts for this execution
+        /// </summary>
         [StorytellerFlag("retries")]
         public int? Retries { get; set; }
     }
